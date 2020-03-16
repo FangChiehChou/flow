bab74414d7384d5966b16e96685e6aba19fbd197
fcchou <choujerry829@gmail.com>
2020-03-16 12:43:40 -0700

    Make the basic LOR code compatible with new flow

--- FLOR/sugiyama_all_controllers_FCC.py
+++ FLOR/sugiyama_all_controllers_FCC.py
@@ -13,15 +13,15 @@ from flow.core.params import SumoParams, EnvParams,InitialConfig, NetParams, Sum
 from flow.core.params import VehicleParams
 from flow.envs.ring.accel import AccelEnv, ADDITIONAL_ENV_PARAMS   # from flow.envs.loop.loop_accel import AccelEnv, ADDITIONAL_ENV_PARAMS
 
-from flow.scenarios.loop import ADDITIONAL_NET_PARAMS
-from flow.networks.ring import RingNetwork
+from flow.networks.ring import RingNetwork, ADDITIONAL_NET_PARAMS
 from flow.core.util import emission_to_csv
 
 import numpy as np
 import os
 
 
-def sugiyama_even_interval(render=True, x=0, cont='OV_FTL'):
+
+def sugiyama_even_interval(render=True, x=0, cont='OV_FTL',emission_path = None):
     """
     Perform a simulation of vehicles on a ring road.
     AVs are placed with 1 HV intervals.
@@ -80,11 +80,6 @@ def sugiyama_even_interval(render=True, x=0, cont='OV_FTL'):
     if x>22:
         return False
 
-    sim_params = SumoParams(sim_step=0.1, render=False, emission_path='/home/lorr/flow/FLOR/IDM_AVRider_EvenInterval_1101/IDM_AVRider_{}_EvenInterval/IDM{}_{}{}'.format(cont,22-x,cont,x))
-    
-    if render is not None:
-        sim_params.render = render
-
     #Switch the controller to be deployed
     if cont == "AUG":
         controller = Augmented_OV_FTL    
@@ -129,123 +124,64 @@ def sugiyama_even_interval(render=True, x=0, cont='OV_FTL'):
             routing_controller=(ContinuousRouter, {}),
             num_vehicles=1)
 
-    env_params = EnvParams(additional_params=ADDITIONAL_ENV_PARAMS)
+    net_params = ADDITIONAL_NET_PARAMS.copy()
+    net_params['length'] = 260
 
-    net_params = NetParams(
-        additional_params={
-            'length': 260,
-            'lanes': 1,
-            'speed_limit': 30,
-            'resolution': 40
-        }
+    flow_params = dict(
+        # name of the experiment
+        exp_tag='ring',
+
+        # name of the flow environment the experiment is running on
+        env_name=AccelEnv,
+
+        # name of the network class the experiment is running on
+        network=RingNetwork,
+
+        # simulator that is used by the experiment
+        simulator='traci',
+
+        # sumo-related parameters (see flow.core.params.SumoParams)
+        sim=SumoParams(
+            render=True,
+            sim_step=0.1,
+        ),
+
+        # environment related parameters (see flow.core.params.EnvParams)
+        env=EnvParams(
+            horizon=6000,
+            additional_params=ADDITIONAL_ENV_PARAMS,
+        ),
+
+        # network-related parameters (see flow.core.params.NetParams and the
+        # network's documentation or ADDITIONAL_NET_PARAMS component)
+        net=NetParams(
+            additional_params=net_params,
+        ),
+
+        # vehicles to be placed in the network at the start of a rollout (see
+        # flow.core.params.VehicleParams)
+        veh=vehicles,
+
+        # parameters specifying the positioning of vehicles upon initialization/
+        # reset (see flow.core.params.InitialConfig)
+        initial=InitialConfig(perturbation=1, spacing='uniform'),
     )
 
-    initial_config = InitialConfig(perturbation=1, spacing='uniform')
+    # Update some variables based on inputs.
+    flow_params['sim'].render = render
+    flow_params['simulator'] = 'traci'
 
-    scenario = RingNetwork(
-        name="sugiyama",
-        vehicles=vehicles,
-        net_params=net_params,
-        initial_config=initial_config)
-
-    env = AccelEnv(env_params, sim_params, scenario)
-
-    return Experiment(env)
-
-
-
-
-def sugiyama_10HV_interval(render=True, cont='OV_FTL'):
-    """
-    Perform a simulation of vehicles on a ring road.
-    2 AVs are placed with 10 HV intervals.
-    """
-    sim_params = SumoParams(sim_step=0.1, render=False, emission_path='/home/lorr/flow/FLOR/IDM_AVRider_{}_10Interval/IDM_{}'.format(cont,cont))
+    # # Specify an emission path if they are meant to be generated.
+    if emission_path is not None:
+        emission_path = emission_path + "/IDM_AVRider_EvenInterval/IDM_AVRider_{}_EvenInterval/IDM{}_{}{}".format(cont,22-x,cont,x)
+        flow_params['sim'].emission_path = emission_path 
     
-    if render is not None:
-        sim_params.render = render
-
-    #Switch the controller to be deployed
-    if cont == "AUG":
-        controller = Augmented_OV_FTL    
-    if cont == "MLYAU1":
-        controller = ModifiedLyapunovTypeControllerU1
-    if cont == "MLYAU2":
-        controller = ModifiedLyapunovTypeControllerU2
-    if cont == "FUZO":
-        controller = FuzzyController_Old
-    if cont == "FUZN":
-        controller = FuzzyController_New
-    if cont == "FS":
-        controller = FollowerStopper
-    if cont == "LACC":
-        controller = LACController    
-    if cont == "PI":
-        controller = PISaturation
-    if cont == "BCM":
-        controller = BCMController
-    if cont == "LinOpt":
-        controller = LinOpt_Controller_IDM
-
-    vehicles = VehicleParams()    
-    vehicles.add(
-        veh_id="IDM1",
-        acceleration_controller=(IDMController, {"noise":0.1}),
-        car_following_params=SumoCarFollowingParams(
-            min_gap=0
-        ),
-        routing_controller=(ContinuousRouter, {}),
-        num_vehicles=10)
-    vehicles.add(
-        veh_id="{}1".format(cont),
-        acceleration_controller=(AVRider, {"AVController":controller}),
-        car_following_params=SumoCarFollowingParams(
-            min_gap=0
-        ),
-        routing_controller=(ContinuousRouter, {}),
-        num_vehicles=1)
-    vehicles.add(
-        veh_id="IDM2",
-        acceleration_controller=(IDMController, {"noise":0.1}),
-        car_following_params=SumoCarFollowingParams(
-            min_gap=0
-        ),
-        routing_controller=(ContinuousRouter, {}),
-        num_vehicles=10)
-    vehicles.add(
-        veh_id="{}2".format(cont),
-        acceleration_controller=(AVRider, {"AVController":controller}),
-        car_following_params=SumoCarFollowingParams(
-            min_gap=0
-        ),
-        routing_controller=(ContinuousRouter, {}),
-        num_vehicles=1)
-
-    env_params = EnvParams(additional_params=ADDITIONAL_ENV_PARAMS)
-
-    net_params = NetParams(
-        additional_params={
-            'length': 260,
-            'lanes': 1,
-            'speed_limit': 30,
-            'resolution': 40
-        }
-    )
-
-    initial_config = InitialConfig(perturbation=1, spacing='uniform')
-
-    scenario = RingNetwork(
-        name="sugiyama",
-        vehicles=vehicles,
-        net_params=net_params,
-        initial_config=initial_config)
-
-    env = AccelEnv(env_params, sim_params, scenario)
-
-    return Experiment(env)
+  
+    return Experiment(flow_params) 
 
 
-def sugiyama_1HV_interval(render=True, x=0, cont='OV_FTL'):
+
+def sugiyama_1HV_interval(render=True, x=0, cont='OV_FTL',emission_path = None):
     """
     Perform a simulation of vehicles on a ring road.
     AVs are placed with 1 HV intervals.
@@ -253,11 +189,6 @@ def sugiyama_1HV_interval(render=True, x=0, cont='OV_FTL'):
     
     if x>11:
         return False
-
-    sim_params = SumoParams(sim_step=0.1, render=False, emission_path='/home/lorr/flow/FLOR/IDM_AVRider_{}_2Interval/IDM{}_{}{}'.format(cont,22-x,cont,x))
-    
-    if render is not None:
-        sim_params.render = render
 
     #Switch the controller to be deployed
     if cont == "AUG":
@@ -311,31 +242,62 @@ def sugiyama_1HV_interval(render=True, x=0, cont='OV_FTL'):
             routing_controller=(ContinuousRouter, {}),
             num_vehicles=22-2*x)    
 
-    env_params = EnvParams(additional_params=ADDITIONAL_ENV_PARAMS)
+    net_params = ADDITIONAL_NET_PARAMS.copy()
+    net_params['length'] = 260
 
-    net_params = NetParams(
-        additional_params={
-            'length': 260,
-            'lanes': 1,
-            'speed_limit': 30,
-            'resolution': 40
-        }
+    flow_params = dict(
+        # name of the experiment
+        exp_tag='ring',
+
+        # name of the flow environment the experiment is running on
+        env_name=AccelEnv,
+
+        # name of the network class the experiment is running on
+        network=RingNetwork,
+
+        # simulator that is used by the experiment
+        simulator='traci',
+
+        # sumo-related parameters (see flow.core.params.SumoParams)
+        sim=SumoParams(
+            render=True,
+            sim_step=0.1,
+        ),
+
+        # environment related parameters (see flow.core.params.EnvParams)
+        env=EnvParams(
+            horizon=6000,
+            additional_params=ADDITIONAL_ENV_PARAMS,
+        ),
+
+        # network-related parameters (see flow.core.params.NetParams and the
+        # network's documentation or ADDITIONAL_NET_PARAMS component)
+        net=NetParams(
+            additional_params=net_params,
+        ),
+
+        # vehicles to be placed in the network at the start of a rollout (see
+        # flow.core.params.VehicleParams)
+        veh=vehicles,
+
+        # parameters specifying the positioning of vehicles upon initialization/
+        # reset (see flow.core.params.InitialConfig)
+        initial=InitialConfig(perturbation=1, spacing='uniform'),
     )
 
-    initial_config = InitialConfig(perturbation=1, spacing='uniform')
+    # Update some variables based on inputs.
+    flow_params['sim'].render = render
+    flow_params['simulator'] = 'traci'
 
-    scenario = RingNetwork(
-        name="sugiyama",
-        vehicles=vehicles,
-        net_params=net_params,
-        initial_config=initial_config)
-
-    env = AccelEnv(env_params, sim_params, scenario)
-
-    return Experiment(env)        
+    # # Specify an emission path if they are meant to be generated.
+    if emission_path is not None:
+        emission_path = emission_path + "/IDM_AVRider_{}_1HVInterval/IDM{}_{}{}".format(cont,22-x,cont,x)
+        flow_params['sim'].emission_path = emission_path 
+ 
+    return Experiment(flow_params) 
 
 
-def sugiyama_example1(render=True, x=0, cont='OV_FTL'):
+def sugiyama_example1(render=True, x=0, cont='PI', emission_path = None):
     """
     Perform a simulation of vehicles on a ring road.
     Parameters
@@ -348,12 +310,8 @@ def sugiyama_example1(render=True, x=0, cont='OV_FTL'):
         A non-rl experiment demonstrating the performance of human-driven
         vehicles on a ring road.
     """
-    
-    sim_params = SumoParams(sim_step=0.1, render=False, emission_path='/home/lorr/flow/FLOR/Sim1025_IDM_AVRider_{}/{}IDM_{}{}'.format(cont,22-x,x,cont))
-    
-    if render is not None:
-        sim_params.render = render
 
+ 
     #Switch the controller to be deployed
     if cont == "AUG":
         controller = Augmented_OV_FTL    
@@ -399,28 +357,79 @@ def sugiyama_example1(render=True, x=0, cont='OV_FTL'):
     # net_params = NetParams(additional_params=additional_net_params)
 
 
-    env_params = EnvParams(additional_params=ADDITIONAL_ENV_PARAMS)
+    # env_params = EnvParams(additional_params=ADDITIONAL_ENV_PARAMS)
+    net_params = ADDITIONAL_NET_PARAMS.copy()
+    net_params['length'] = 260
+    # net_params = NetParams(
+    #     additional_params={
+    #         'length': 260,
+    #         'lanes': 1,
+    #         'speed_limit': 30,
+    #         'resolution': 40
+    #     }
+    # )
 
-    net_params = NetParams(
-        additional_params={
-            'length': 260,
-            'lanes': 1,
-            'speed_limit': 30,
-            'resolution': 40
-        }
+    # initial_config = InitialConfig(perturbation=1, spacing='uniform')
+
+    # scenario = RingNetwork(
+    #     name="sugiyama",
+    #     vehicles=vehicles,
+    #     net_params=net_params,
+    #     initial_config=initial_config)
+
+    # env = AccelEnv(env_params, sim_params, scenario)
+
+
+    flow_params = dict(
+        # name of the experiment
+        exp_tag='ring',
+
+        # name of the flow environment the experiment is running on
+        env_name=AccelEnv,
+
+        # name of the network class the experiment is running on
+        network=RingNetwork,
+
+        # simulator that is used by the experiment
+        simulator='traci',
+
+        # sumo-related parameters (see flow.core.params.SumoParams)
+        sim=SumoParams(
+            render=True,
+            sim_step=0.1,
+        ),
+
+        # environment related parameters (see flow.core.params.EnvParams)
+        env=EnvParams(
+            horizon=6000,
+            additional_params=ADDITIONAL_ENV_PARAMS,
+        ),
+
+        # network-related parameters (see flow.core.params.NetParams and the
+        # network's documentation or ADDITIONAL_NET_PARAMS component)
+        net=NetParams(
+            additional_params=net_params,
+        ),
+
+        # vehicles to be placed in the network at the start of a rollout (see
+        # flow.core.params.VehicleParams)
+        veh=vehicles,
+
+        # parameters specifying the positioning of vehicles upon initialization/
+        # reset (see flow.core.params.InitialConfig)
+        initial=InitialConfig(perturbation=1, spacing='uniform'),
     )
 
-    initial_config = InitialConfig(perturbation=1, spacing='uniform')
+    # Update some variables based on inputs.
+    flow_params['sim'].render = render
+    flow_params['simulator'] = 'traci'
 
-    scenario = RingNetwork(
-        name="sugiyama",
-        vehicles=vehicles,
-        net_params=net_params,
-        initial_config=initial_config)
-
-    env = AccelEnv(env_params, sim_params, scenario)
-
-    return Experiment(env)
+    # # Specify an emission path if they are meant to be generated.
+    if emission_path is not None:
+        emission_path = emission_path + "/IDM_AVRider_{}/IDM{}_{}{}".format(cont,22-x,cont,x)
+        flow_params['sim'].emission_path = emission_path #"./data"
+    
+    return Experiment(flow_params)
 
 """-------------------------------------------------------------"""
 
@@ -428,15 +437,15 @@ import random
 
 if __name__ == "__main__":
     #==================Run One Controller =================
-    # AV_case = ['AUG','MLYAU1','MLYAU2','FUZN','FUZO','LACC','PI','FS','BCM','LinOpt']
-    # import the experiment variable
-    # NumAV = 1
-    # TypeAV = 'PI'
-    # exp = sugiyama_example1(x = NumAV, cont=TypeAV, render=False)
-    # exp = sugiyama_10HV_interval(render=True, cont=TypeAV)
+    AV_case = ['AUG','MLYAU1','MLYAU2','FUZN','FUZO','LACC','PI','FS','BCM','LinOpt']
+    ##import the experiment variable
+    NumAV = 1
+    TypeAV = 'PI'
+    exp = sugiyama_example1(x = NumAV, cont=TypeAV, render=True, emission_path = "./data") 
     # exp = sugiyama_1HV_interval(render=False, x=NumAV, cont=TypeAV)
-    # run for a set number of rollouts / time steps
+    ### run for a set number of rollouts / time steps
     # exp.run(1, 6000)
+    exp.run(1, convert_to_csv=True)  
 
     #==================Run all controllers (10 HVs interval) in a bacth=================
     ## 
@@ -474,16 +483,16 @@ if __name__ == "__main__":
 
 
 ##    ==================Run all controllers evenly distributed in a bacth=================
-    AV_case = ['AUG','MLYAU1','MLYAU2','FUZN','FUZO','LACC','PI','FS','BCM','LinOpt']
+    # AV_case = ['AUG','MLYAU1','MLYAU2','FUZN','FUZO','LACC','PI','FS','BCM','LinOpt']
     
-    for av in AV_case:
-        print('Start {}'.format(av)) 
-        AV_num = 1
-        for x in range(2,12):
-            AV_num = AV_num + 1
-            exp1 = sugiyama_even_interval(x = x, cont=av, render=False)
-            exp1.run(1, 6000, convert_to_csv=True)
-            print('{} {} placed on the ring, {} iterations left'.format(AV_num,av,11-AV_num)) 
-            print('done') 
-            del exp1
+    # for av in AV_case:
+    #     print('Start {}'.format(av)) 
+    #     AV_num = 1
+    #     for x in range(2,12):
+    #         AV_num = AV_num + 1
+    #         exp1 = sugiyama_even_interval(x = x, cont=av, render=False)
+    #         exp1.run(1, 6000, convert_to_csv=True)
+    #         print('{} {} placed on the ring, {} iterations left'.format(AV_num,av,11-AV_num)) 
+    #         print('done') 
+    #         del exp1
 
